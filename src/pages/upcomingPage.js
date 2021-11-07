d2361cbdd438a12244327e10d1be48bff6da9869
Rosanne Birney <rbirney@wit.ie>
2021-11-07 13:33:13 +0000

    Change upcoming movies page to use caching.

--- src/pages/upcomingPage.js
+++ src/pages/upcomingPage.js
@@ -1,19 +1,21 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage'
+import React from "react";
+import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist';
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
 
   return (
     <PageTemplate
